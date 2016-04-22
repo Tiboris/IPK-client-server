cd9eb0c6f8c8dd2266ba4309ee37cba792171115
Tiboris <xdudla00@vutbr.cz>
2016-04-22 23:44:41 +0200

    Arguments parsed

--- client.cpp
+++ client.cpp
@@ -8,219 +8,143 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-/*
-*
-*/
+#include <stdlib.h>
+
 using namespace std;
+/*
+*   Structure for options
+*/
+typedef struct Options
+{
+    string hostname;
+    string file_name;
+    int port;
+    bool download;
+} options;
+
 /*
 *   Used constants and variables
 */
-const int U_PORT = 0;
-const int U_PATH = 1;
-const int F_NAME = 2;
-const int REQ_ARGC = 2;
+const int REQ_ARGC = 3;
 const int MAX_ATTEMPTS = 5;
 const size_t BUFF_SIZE = 1024;
-const string DEF_PORT = "80";
-const string DEF_PATH = "/";
-const string DEF_NAME = "index.html";
-string file_name;
+string file_name = "";
+
 string known_urls = "";
 int attempts = 0; 
 /*
 * Used regular expressions
 */
-const regex url_rex("^http://(www\\.)?([\\w\\.-]+)(\\:(\\d+))?(\\/.*)?$");
-const regex head_rex("HTTP\\/1.(\\d) (\\d+) (.*)");
-const regex chunk_rex("Transfer-Encoding: chunked");
-const regex locattion_rex("Location: (.*)");
-const regex space(" ");
-const regex tilda("~");
+
 /*
 * Prototypes of functions
 */
 void err_print(const char *msg);
-string get_default(string str, int type);
-smatch get_rex_matches(string url, regex rex);
-string get_cont(int sckt, string url, string file_path, string ver);
-bool connect_to(string url,string port,string path, int attempts, string ver="1");
-bool save_response(string response, bool chunked);
+
+bool args_err(int argc, char** argv, options* args);
+
+string get_cont();
+
+bool connect_to();
+
+bool save_response();
 /*
 *   Main
 */
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {   
-    if ( ! ( argc == REQ_ARGC ) ) {
-        err_print("Wrong parameters, usage: ./webclient url");
+    options args;
+    if ( args_err(argc,argv,&args) ) 
+    {
+        err_print("Wrong parameters, usage: ./client -h hostname -p port [-­d|u] file_name");
         return EXIT_FAILURE;
     }
-    if ( ! regex_match(argv[1], url_rex) ) {
-        err_print("Bad url");
+    cout << "host " << args.hostname << endl;
+    if ( connect_to() ) 
+    {
         return EXIT_FAILURE;
     }
-    string url = argv[1];
-    smatch url_parts;
-    regex_match(url,url_parts,url_rex);// [0]full_url [1]www [2]domain [4]port [5]path
-    const string path = get_default(static_cast<string>(url_parts[5]), U_PATH);
-    const string port = get_default(static_cast<string>(url_parts[4]),U_PORT );
-    url  = static_cast<string>(url_parts[1]) + static_cast<string>(url_parts[2]);
-    if ( connect_to(url, port, path, attempts) ) {
+
+    return EXIT_SUCCESS;
+}
+/*
+*   For printing errors
+*/
+void err_print(const char *msg)
+{
+    cerr << msg << endl;    
+}
+/*
+*
+*/
+bool args_err(int argc, char** argv, options* args)
+{
+    /*
+    client -h eva.fit.vutbr.cz -p 10000 -u test.txt
+    provede nahrání souboru test.txt z aktuálního adresáře procesu klienta do
+    aktuálního adresáře procesu serveru
+    */
+    int opt,h=0,p=0,u=0,d=0;
+    while ((opt = getopt(argc,argv,"h:p:u:d:")) != EOF)
+    {
+        switch(opt)
+        {
+            case 'h': 
+            {
+                h++; 
+                args->hostname = optarg ; 
+                break;
+            }
+            case 'u':
+            { 
+                u++; 
+                args->file_name = optarg; 
+                break;
+            }
+            case 'd': 
+            {
+                d++; 
+                args->file_name = optarg; ; 
+                break;
+            }
+            case 'p': 
+            {
+                p++; 
+                char* garbage = '\0';
+                int port = strtoul(optarg,&garbage,0);
+                if ((garbage[0]!='\0') || (port < 0)) 
+                {
+                    return EXIT_FAILURE;
+                }
+                args->port = port; 
+                break;
+            }
+            case '?': 
+            {
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    if ((h > 1) || (p > 1) || (u > 1) || (d > 1) || ((h+p+d+u) != REQ_ARGC))
+    {
         return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
 /*
 *   For connection handling
 */
-bool connect_to(string url,string port,string path, int attempts, string ver)
+bool connect_to()
 {   
-    size_t pos = path.find_last_of("/");
-    if (attempts == 0) {
-        file_name = get_default(path.substr(pos+1), F_NAME );
-    }
-    if (attempts > MAX_ATTEMPTS) {
-        err_print("Too many redirects");
-        return EXIT_FAILURE;
-    } 
-    string to_find = "^"+url+">>>>";
-    size_t jump;
-    if ( (jump = known_urls.find(to_find)) != string::npos) {
-        string cut = known_urls.substr(jump);
-        jump = to_find.size();
-        cut=cut.substr(jump);
-        jump = cut.find_first_of("|");
-        string found_url="http://"+cut.substr(0,jump)+path;
-        smatch url_part;
-        regex_match(found_url,url_part,url_rex);
-        path = get_default(static_cast<string>(url_part[5]), U_PATH);
-        port = get_default(static_cast<string>(url_part[4]),U_PORT );
-        url  = static_cast<string>(url_part[1]) + static_cast<string>(url_part[2]);
-    }
-
-    int adr_code, resp_code, sckt;
-    struct addrinfo set;
-    struct addrinfo *results, *res;
-
-    memset(&set, 0, sizeof(struct addrinfo));
-    set.ai_socktype = SOCK_STREAM; /* TCP Socket */
-    set.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
-    set.ai_protocol = 0;           /* Any protocol */
-    set.ai_flags = 0;
-
-    if ((adr_code = getaddrinfo( url.c_str(), port.c_str(), &set, &results) ) != 0) { 
-        err_print( gai_strerror(adr_code) );
-        return EXIT_FAILURE;
-    }
-    for (res = results; res != NULL; res = res->ai_next)  
-    {   // connecting to translated addresses
-        sckt = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-        if (sckt == -1) {
-            continue;
-        }
-        if (connect(sckt, res->ai_addr, res->ai_addrlen) != -1) {
-            freeaddrinfo(results);  // free no longer needed 
-            break;                  // On Successfull connection
-        }
-        close(sckt);
-    }
-    if (res == NULL){
-        err_print("Could not connect");
-        return EXIT_FAILURE;
-    }
-    // Saving response
-    string resp_msg = get_cont(sckt, url, path, ver);
-    if (resp_msg == "ErrInMyGet") {
-        return EXIT_FAILURE;
-    }
-    // Iterator for regex output
-    smatch head;
-    regex_search(resp_msg, head, head_rex);
-    string res_ver = static_cast<string>(head[1]);
-    string s_code = static_cast<string>(head[2]);
-    resp_code = atoi(s_code.c_str());
-    string err_msg = static_cast<string>(head[3]);
-    bool chunked = regex_search(resp_msg, head, chunk_rex);
-    close(sckt);    // closing socket
-    if (resp_code == 200) { // OK
-        if ( res_ver == ver ) {
-            if (save_response(resp_msg, chunked)){
-                return EXIT_FAILURE;
-            }
-            return EXIT_SUCCESS;
-        }
-        if ( res_ver != ver ) {
-            if( connect_to(url,port, path, attempts, "0") ){
-                return EXIT_FAILURE;
-            }
-            return EXIT_SUCCESS;
-        }        
-    }
-    if (resp_code == 302 || resp_code == 301) { // REDIRECT
-        regex_search(resp_msg, head, locattion_rex);
-        string location = static_cast<string>(head[1]);
-        string head_location = location;
-        smatch url_parts;
-        regex_match(location,url_parts,url_rex);
-        path = get_default(static_cast<string>(url_parts[5]), U_PATH);
-        port = get_default(static_cast<string>(url_parts[4]), U_PORT);
-        location = static_cast<string>(url_parts[1]) + static_cast<string>(url_parts[2]);
-        if (location == "") { // if regex matched bad url after location 
-            err_print(("Bad "+s_code+" Location in HEAD response: "+head_location).c_str());
-            return EXIT_FAILURE;
-        }
-        if (resp_code == 301 ) { // saving url to cache
-            if ( known_urls.find("^"+url+">>>>"+location+"|\n") == string::npos ){
-                known_urls.append("^"+url+">>>>"+location+"|\n");
-            }        
-        }
-        if (connect_to(location,port,path, ++attempts) ) { 
-            return EXIT_FAILURE;
-        }
-        return EXIT_SUCCESS;
-    }
-    if ((resp_code==400) && (res_ver != ver)) { // BAD REQUEST
-        if( connect_to(url,port, path, attempts, "0") ){
-            return EXIT_FAILURE;
-        }
-        return EXIT_SUCCESS;
-    }
-    if (resp_code > 399 && resp_code < 600) { // OTHER ERRORS
-        err_print((s_code + " " + err_msg).c_str());
-        return EXIT_FAILURE;
-    }
-    // OTHER NOT KNOWN ERRORS
-    err_print(( "ERR : " + s_code + " " + err_msg).c_str());
-    return EXIT_FAILURE;
+    return EXIT_SUCCESS;
 }
 /*
 *   For sending head request and returning response
 */
-string get_cont( int sckt, string url, string file_path, string ver){
-    int res;
-    char response[BUFF_SIZE];
-    bzero(response, BUFF_SIZE);
-    string resp_msg = "";
-    string replacement[2] = { "%20","%7E"};
-    file_path  = regex_replace(file_path,space,replacement[0]);
-    file_path  = regex_replace(file_path,tilda,replacement[1]);
-    string msg =    "GET " + file_path + " HTTP/1." + ver + "\r\n" + 
-                    "Host: " + url + "\r\n" + "Connection: close\r\n\r\n";
-    if ( (send(sckt, msg.c_str(),msg.size(), 0)) < 1 ) {
-        err_print("Error while sending request");
-        return "ErrInMyGet";
-    }
-    while ( (res = recv(sckt, response, BUFF_SIZE-1, 0 )) )
-    {
-        if (res < 1) {
-            err_print("Error while getting sesponse");
-            return "ErrInMyGet";
-        }
-        else { // response get 
-            resp_msg.append(response, res);
-            bzero(response, BUFF_SIZE); // buff erase
-        }
-    }
-    return resp_msg;
+string get_cont()
+{
+    return EXIT_SUCCESS;
 }
 /*
 *   For writing into file
@@ -229,7 +153,8 @@ bool save_response(string response, bool chunked)
 {
     size_t pos = response.find("\r\n\r\n");
     response.erase(0, pos+4);
-    if (chunked) {
+    if (chunked) 
+    {
         string chunked_resp = response;
         response = "";
         unsigned int block_size;
@@ -246,24 +171,4 @@ bool save_response(string response, bool chunked)
     myfile << response;
     myfile.close();
     return EXIT_SUCCESS;
-}
-/*
-*   For getting Default values
-*/
-string get_default(string str, int type)
-{
-    switch (type) 
-    {
-        case U_PORT : return (str.size() != 0) ? str : DEF_PORT ;
-        case F_NAME : return (str.size() != 0) ? str : DEF_NAME ;
-        case U_PATH : return (str.size() != 0) ? str : DEF_PATH ;
-    }
-    return NULL;
-}
-/*
-*   For printing errors
-*/
-void err_print(const char *msg)
-{
-    cerr << msg << endl;    
 }
--- server.cpp
+++ server.cpp
@@ -15,25 +15,70 @@ using namespace std;
 /*
 *   Used constants and variables
 */
-
-/*
-* Used regular expressions
-*/
-
+const int REQ_ARGC = 3;
+const int MAX_CLIENTS = 10;
 /*
 * Prototypes of functions
 */
 void err_print(const char *msg);
+bool args_err(int argc, const char** argv);
+
 /*
 *   Main
 */
 int main(int argc, char const *argv[])
 {   
-    if ( ! ( argc != 1 ) ) {
-        err_print("Wrong parameters, usage: ./webclient url");
+    if (args_err(argc,argv)) 
+    {
+        err_print("Wrong parameters, usage: ./server -p [port_number]");
         return EXIT_FAILURE;
     }
-    cout<<argv[0]<<endl;
+    int port = strtoul(argv[2],NULL,0);
+    /*
+        vars
+    */
+    int sockfd, newsockfd, clilen, n;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    /* 
+        First call socket() function 
+    */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+    {
+        err_print("ERROR opening socket");
+        return EXIT_FAILURE;
+    }
+    /* 
+        Initialize socket structure 
+    */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(port);
+    /*
+        Binding socket
+    */
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    {
+        err_print("ERROR on binding, make sure port is available");
+        return EXIT_FAILURE;
+    }
+    /* 
+        Now start listening for the clients, here
+        process will go in sleep mode and will wait
+        for the incoming connection
+    */
+    listen(sockfd,MAX_CLIENTS);
+    clilen = sizeof(cli_addr);
+    /*
+        Getting thing work
+    */
+    while(true)
+    {
+        cout << "Port No: " << port << endl;
+        break;
+    }
     return EXIT_SUCCESS;
 }
 /*
@@ -42,4 +87,21 @@ int main(int argc, char const *argv[])
 void err_print(const char *msg)
 {
     cerr << msg << endl;    
+}
+/*
+*   For checking argument
+*/
+bool args_err(int argc, const char** argv)
+{
+    if ( ( argc != REQ_ARGC ) || (strcmp(argv[1],"-p") != 0)) 
+    {
+        return EXIT_FAILURE;
+    }
+    char* garbage = '\0';
+    int port = strtoul(argv[2],&garbage,0);
+    if ((garbage[0]!='\0') || (port < 0)) 
+    {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
